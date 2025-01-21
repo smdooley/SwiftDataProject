58628b64d67dece520d7bde827e94f659a23bbe5
Sean Dooley <s.dooley@ctidigital.com>
2025-01-21 15:18:09 +0000

    Day 57 Filtering query using predicate

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -11,12 +11,23 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     
-    @Query(sort: \User.name) var users: [User]
+    //@Query(sort: \User.name) var users: [User]
+    
+    // Filter by the letter "R"
+//    @Query(filter: #Predicate<User> { user in
+//        user.name.localizedStandardContains("R")
+//    }, sort: \User.name) var users: [User]
+    
+    // Filter by the letter "R" and lives in "London"
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("R") &&
+        user.city == "London"
+    }, sort: \User.name) var users: [User]
     
     @State private var path = [User]()
     
     var body: some View {
-        NavigationStack(path: $path) {
+        /*NavigationStack(path: $path) {
             List(users) { user in
                 NavigationLink(value: user) {
                     Text(user.name)
@@ -31,6 +42,29 @@ struct ContentView: View {
                     let user = User(name: "", city: "", joinDate: .now)
                     modelContext.insert(user)
                     path = [user]
+                }
+            }
+        }*/
+        
+        NavigationStack {
+            List(users) { user in
+                Text(user.name)
+            }
+            .navigationTitle("Users")
+            .toolbar {
+                Button("Add Samples", systemImage: "plus") {
+                    // Delete existing data
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
