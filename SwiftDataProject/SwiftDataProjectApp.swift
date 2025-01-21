2ae3d40415fa70d9b41aa22ae667870e29d1c543
Sean Dooley <s.dooley@ctidigital.com>
2025-01-21 15:04:52 +0000

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Sean Dooley on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
